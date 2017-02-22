69d75a53b2a106bc3349fec4d3d541cf5c833618
qicaitiankong <zihaoshuhan@163.com>
2017-02-22 21:31:02 +0800

    老一提交2

---
+++ view1.h
@@ -0,0 +1,13 @@
+//
+//  view1.h
+//  gitTest
+//
+//  Created by 李自豪 on 2017/2/22.
+//  Copyright © 2017年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface view1 : UIView
+
+@end
---
+++ view1.m
@@ -0,0 +1,30 @@
+//
+//  view1.m
+//  gitTest
+//
+//  Created by 李自豪 on 2017/2/22.
+//  Copyright © 2017年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import "view1.h"
+
+@implementation view1
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+            
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
