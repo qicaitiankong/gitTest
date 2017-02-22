47a76a47880cf8afddcd24acd659838c588e3a2b
qicaitiankong <zihaoshuhan@163.com>
2017-02-22 20:55:56 +0800

    添加了文件爱

---
+++ gitTest/test2.h
@@ -0,0 +1,13 @@
+//
+//  test2.h
+//  gitTest
+//
+//  Created by 李自豪 on 2017/2/22.
+//  Copyright © 2017年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test2 : UIView
+
+@end
---
+++ gitTest/test2.m
@@ -0,0 +1,21 @@
+//
+//  test2.m
+//  gitTest
+//
+//  Created by 李自豪 on 2017/2/22.
+//  Copyright © 2017年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import "test2.h"
+
+@implementation test2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
